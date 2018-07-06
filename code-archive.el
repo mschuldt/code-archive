735ba47be3db59d9af784f2a47f8585caa55a6fd
mschuldt <mbschuldt@gmail.com>
2018-07-05 19:10:00 -0700

    update description, commentary, and readme

--- code-archive.el
+++ code-archive.el
@@ -1,9 +1,9 @@
-;;; code-archive.el ---  Archive and reference source code.   -*- lexical-binding: t -*-
+;;; code-archive.el --- git supported code archive and reference for org-mode -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2018 Michael Schuldt
 
 ;; Author: Michael Schuldt <mbschuldt@gmail.com>
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/mschuldt/code-archive
 
@@ -21,9 +21,24 @@
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; Archive and reference source code snippets.
-
+;; 
+;; Archive and reference source code snippets
+;; 
+;; This package provides commands for saving selecting code regions and
+;; inserting them as org-mode styled code blocks. These code blocks are
+;; tagged with an id that allows jumping back to the original source file.
+;; The original source file is archived in a git managed repo each time
+;; a code block is saved.
+;; 
+;; Saving full file copies enables referencing the orignal context and
+;; avoids the problem of locating bookmarked regions when the file becomes
+;; massively mutated or deleted. Using git solves the problem of saving
+;; multiple versions of a file history in a space efficient way.
+;; 
+;; Additional helpers are provided for org-capture templates.
+;; 
+;; See the README for usage at https://github.com/mschuldt/code-archive
+;; 
 ;;; Code:
 
 (require 'cl)
